200eaca749c67e979ca37b95c9bdcd86e04a26bb
Jason Cheung <jasoncheungcf@gmail.com>
2015-09-01 15:27:21 -0700

    Initial Commit

---
+++ MultipleStoryBoardSegue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultipleStoryBoardSegue
+//
+//  Created by Jason Cheung on 2015-09-01.
+//  Copyright (c) 2015 JCheungX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
